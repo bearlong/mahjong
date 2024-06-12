fe2d53cc284b3fcf6202821d6d617e2eeb7640d1
Bear <a86774546@gmail.com>
2024-06-12 23:57:03 +0800

    課程章節修改

--- mjphp/course-chapter.php
+++ mjphp/course-chapter.php
@@ -43,18 +43,41 @@ $rowCourse = $resultCourse->fetch_assoc();
 </head>
 
 <body>
+    <button type="button" class="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal" id="modalTriggerButton">
+        Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5 fw-semibold" id="exampleModalLabel">課程尚未有章節</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    請依序填入章節名稱、簡述、影片及預覽圖。
+                    <br>
+                    按下"+"號可新增下一個章節。
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <h1>章節編輯</h1>
         <button class="btn btn-primary my-2" id="plus"><i class="fa-solid fa-plus"></i></button>
-        <form action="">
-            <table class="table table-bordered" id="form-td">
-                <tr>
-                    <th>章節名稱</th>
-                    <th>章節簡述</th>
-                    <th>上傳影片</th>
-                    <th>章節圖</th>
-                </tr>
-                <?php if ($resultChap->num_rows == 0) : ?>
+        <table class="table table-bordered" id="form-td">
+            <tr>
+                <th>章節名稱</th>
+                <th>章節簡述</th>
+                <th>上傳影片</th>
+                <th>章節圖</th>
+            </tr>
+            <?php if ($resultChap->num_rows == 0) : ?>
+                <form action="doAddChapter.php" method="post" enctype="multipart/form-data">
                     <tr>
                         <td>
                             <input type="text" name="name" class="form-control">
@@ -66,41 +89,50 @@ $rowCourse = $resultCourse->fetch_assoc();
                             <input type="file" name="video" class="form-control">
                         </td>
                         <td>
-                            <input type="file" name="image" class="form-control">
+                            <input type="file" name="image" class="form-control uploadImage">
                         </td>
                         <td>
                             預覽圖:
-                            <img src="" alt="">
+                            <img class="img-thumbnail image d-none" src="" alt="">
                         </td>
                     </tr>
                 <?php else : ?>
                     <?php foreach ($rows as $chapter) : ?>
-                        <tr>
-                            <td>
-                                <input type="text" name="name" class="form-control" value="<?= $chapter["chapter_name"] ?>">
-                            </td>
-                            <td>
-                                <textarea name="content" class="form-control" rows="3"><?= $chapter["content"] ?></textarea>
-                            </td>
-                            <td>
-                                <input type="file" name="video" class="form-control">
-                            </td>
-                            <td>
-                                <input type="file" name="image" class="form-control uploadImage">
-                            </td>
-                            <td>
-                                預覽圖:
-                                <img class="img-thumbnail image" src="./images/Mahjong/<?= $chapter["images"] ?>" alt="">
-
-                            </td>
-                        </tr>
+                        <form action="doEditChapter.php" method="post" enctype="multipart/form-data">
+                            <tr>
+                                <td>
+                                    <input type="text" name="name" class="form-control" value="<?= $chapter["chapter_name"] ?>">
+                                </td>
+                                <td>
+                                    <textarea name="content" class="form-control" rows="3"><?= $chapter["content"] ?></textarea>
+                                </td>
+                                <td>
+                                    <input type="file" name="video" class="form-control">
+                                </td>
+                                <td>
+                                    <input type="file" name="image" class="form-control uploadImage">
+                                </td>
+                                <td>
+                                    預覽圖:
+                                    <img class="img-thumbnail image" src="./images/Mahjong/<?= $chapter["images"] ?>" alt="">
+                                </td>
+                                <td>
+                                    <button class="btn btn-primary my-3"><i class="fa-solid fa-pen-to-square"></i></button>
+                                </td>
+                            </tr>
+                        </form>
                     <?php endforeach; ?>
-                <?php endif; ?>
+                    <form action="doAddChapter.php" method="post" enctype="multipart/form-data">
 
-            </table>
+                    <?php endif; ?>
+
+        </table>
+        <button class="btn btn-primary my-3">送出</button>
         </form>
     </div>
     <!-- Bootstrap JavaScript Libraries -->
+    <?php include("../js-mahjong.php") ?>
+
     <script>
         const plus = document.querySelector("#plus");
         const formTd = document.querySelector("#form-td");
@@ -122,8 +154,15 @@ $rowCourse = $resultCourse->fetch_assoc();
                 });
             }
         });
+
+        <?php if ($resultChap->num_rows == 0) : ?>
+            var modalTriggerButton = document.getElementById('modalTriggerButton');
+            if (modalTriggerButton) {
+                modalTriggerButton.click();
+            }
+
+        <?php endif; ?>
     </script>
-    <?php include("../js-mahjong.php") ?>
 
 </body>
 
